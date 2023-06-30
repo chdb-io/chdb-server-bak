99c213d4214053c265312dc11c1e7c4ddedd1d87
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-07-01 00:57:15 +0200

    Update lambda.py

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,16 +1,15 @@
-import os
 import json
 import chdb
-        
+
 def lambda_handler(event, context):
-    json_region = os.environ['AWS_REGION']
     query = event['query'] or "SELECT version()"
     format = event['default_format'] or "JSONCompact"
     res = chdb.query(query, format)
+    out = json.loads(res.data())
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps(res.get_memview().tobytes())
+        "body": json.dumps(out)
     }
