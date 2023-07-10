08f6b56bb1133ea3c786035c72935dc2877ea2fe
laodouya <xujin3@xunlei.com>
2023-07-10 16:36:39 +0800

    Catch chdb errmsg from stderr and return

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-from flask import Flask, request
-import chdb
 import os
+import select
+
+import chdb
+from flask import Flask, request
 
 app = Flask(__name__, static_folder="public", static_url_path="")
+
+
+def chdb_query_with_errmsg(query, format):
+    pipe_out, pipe_in = os.pipe()
+    stderr = os.dup(2)
+    os.dup2(pipe_in, 2)
+
+    # check if we have more to read from the pipe
+    def more_data():
+        r, _, _ = select.select([pipe_out], [], [], 0)
+        return bool(r)
+
+    # read the whole pipe
+    def read_pipe():
+        out = b''
+        while more_data():
+            out += os.read(pipe_out, 1024)
+
+        return out.decode(encoding='utf-8', errors='strict')
+
+    res = chdb.query(query, format)
+    os.dup2(stderr, 2)
+
+    result = res.get_memview().tobytes()
+    errmsg = read_pipe()
+    return result, errmsg
+
 
 @app.route('/', methods=["GET"])
 def clickhouse():
@@ -11,8 +40,12 @@ def clickhouse():
     if not query:
         return app.send_static_file('play.html')
 
-    res = chdb.query(query, format)
-    return res.get_memview().tobytes()
+    result, errmsg = chdb_query_with_errmsg(query, format)
+    if errmsg != '':
+        return result
+    else:
+        return errmsg
+
 
 @app.route('/', methods=["POST"])
 def play():
@@ -21,12 +54,17 @@ def play():
     if not query:
         return app.send_static_file('play.html')
 
-    res = chdb.query(query, format)
-    return res.get_memview().tobytes()
+    result, errmsg = chdb_query_with_errmsg(query, format)
+    if errmsg != '':
+        return result
+    else:
+        return errmsg
+
 
 @app.errorhandler(404)
 def handle_404(e):
     return app.send_static_file('play.html')
+
 
 host = os.getenv('HOST', '0.0.0.0')
 port = os.getenv('PORT', 8123)
