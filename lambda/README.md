03ffd761ff4cfd730800c622b554dbe004a9b109
Alastair Appleby <ali.appleby@live.co.uk>
2023-08-02 01:30:49 +0200

    Fixed lambda

--- lambda/README.md
+++ lambda/README.md
@@ -26,29 +26,24 @@ curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" \
 <br>
 
 ## Upload Docker image on ECR and Lambda
-Lambda function continers must be hosted on the AWS Elastic Container Registry.
+Lambda function containers must be hosted on the AWS Elastic Container Registry.
 
-1. Export your AWS account id in the shell or better yet, add it your ~/.bashrc or ~/.bash_profile 
-```
-$ export AWS_ACCOUNT_ID = <account_id>
-```
-
-2. Install the AWS CLI and configure with your AWS credentials
+1. Install the AWS CLI and configure with your AWS credentials
 ```
 $ aws configure
 ```
 
-3. Review and execute the ‘deploy.sh’ script:
+2. Review and execute the ‘deploy.sh’ script:
 ```
-$ ./deploy.sh
+$ ./deploy.sh [--tag <value>] [--region <value>] [--profile <default>] [--no-push]
 ```
 
-4. Create Lambda function and attach your ECR Image. Make sure the name and image ID match:
+3. Create Lambda function and attach your ECR Image. Make sure the name and image ID match:
 
 ![image](https://github.com/chdb-io/chdb-server/assets/1423657/887894c3-35ef-4083-a4b8-29d247f1fc1c)
 
 
-6. Test your Lambda function with a JSON payload:
+4. Test your Lambda function with a JSON payload:
 
 ![image](https://github.com/chdb-io/chdb-server/assets/1423657/daa26b0b-68e2-4cec-b665-5505efe99b99)
 
--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -1,14 +1,58 @@
-URL_STRING=".dkr.ecr.us-east-1.amazonaws.com"
-CONTAINER_STRING="chdb"
-IMAGE_STRING="latest"
-ECR_IMAGE_URI="$AWS_ACCOUNT_ID$URL_STRING/$CONTAINER_STRING:$IMAGE_STRING"
+# get flag variables
+profile="default"
+region="us-east-1"
+tag="latest"
+no_push=false
+
+while (( "$#" )); do
+  case "$1" in
+    --tag)
+      tag="$2"
+      shift 2
+      ;;
+    --region)
+      region="$2"
+      shift 2
+      ;;
+    --profile)
+      profile="$2"
+      shift 2
+      ;;
+    --no-push)
+      no_push=true
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    -*|--*=)
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+
+# set variables
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+ECR_IMAGE_URI="$AWS_ACCOUNT_ID.dkr.ecr.$region.amazonaws.com"
+IMAGE_NAME="$ECR_IMAGE_URI/chdb:$tag"
+
 # log in to ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID$URL_STRING"
-# remove previous images to save space
-docker rmi "$AWS_ACCOUNT_ID$URL_STRING/$CONTAINER_STRING"
-docker rmi "$CONTAINER_STRING"
+aws ecr get-login-password --region $region --profile $profile | \
+  docker login --username AWS --password-stdin $ECR_IMAGE_URI
+
+# remove existing image
+docker rmi $IMAGE_NAME 2>/dev/null || true
+
 # build image
-docker build --tag "$CONTAINER_STRING" .
-# tag and push to AWS ECR
-docker tag $CONTAINER_STRING:latest "$ECR_IMAGE_URI"
-docker push "$ECR_IMAGE_URI"
+docker build -t $IMAGE_NAME .
+
+if [ "$no_push" = false ]; then
+    # push to ECR
+    docker push $IMAGE_NAME
+fi
