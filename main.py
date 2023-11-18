e6c65a11c04364ed53227fc3d8e85d655fb72c97
root <root@localhost.localdomain>
2023-11-18 20:09:43 +0100

    post data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tempfile
 
 import chdb
@@ -14,34 +15,52 @@ driver = chdb
 @auth.verify_password
 def verify(username, password):
     if not (username and password):
-        print('stateless session')
+        # print('stateless session')
         globals()["driver"] = chdb
     else:
-        path = globals()["path"] + "/" + str(hash(username + password))
-        print('stateful session ' + path)
+        path = globals()["path"] + "/" + str(username)
+        # print('stateful session ' + path)
         globals()["driver"] = chs.Session(path)
     return True
 
 # run chdb.query(query, format), get result from return and collect stderr
-def chdb_query_with_errmsg(query, format):
-    # Redirect stdout and stderr to the buffers
+def chdb_query_with_errmsg(query, format, data=None):
+    output = ""
+    errmsg = ""
     try:
-        new_stderr = tempfile.TemporaryFile()
+        # Redirect stderr to a temporary file
+        stderr_tempfile = tempfile.TemporaryFile()
         old_stderr_fd = os.dup(2)
-        os.dup2(new_stderr.fileno(), 2)
+        os.dup2(stderr_tempfile.fileno(), 2)
+
+        # Redirect stdin to a temporary file if data is provided
+        if data is not None and query is not None:
+            stdin_tempfile = tempfile.NamedTemporaryFile()
+            # print(stdin_tempfile.name)
+            old_stdin_fd = os.dup(0)
+            os.dup2(stdin_tempfile.fileno(), 0)
+            stdin_tempfile.write(data)
+            stdin_tempfile.seek(0)
+
         # Call the function
         output = driver.query(query, format).bytes()
 
-        new_stderr.flush()
-        new_stderr.seek(0)
-        errmsg = new_stderr.read()
+        # Capture stderr output
+        stderr_tempfile.flush()
+        stderr_tempfile.seek(0)
+        errmsg = stderr_tempfile.read()
 
-        # cleanup and recover
-        new_stderr.close()
-        os.dup2(old_stderr_fd, 2)
     except Exception as e:
-        # An error occurred, print it to stderr
-        print(f"An error occurred: {e}")
+        errmsg = str(e)
+
+    finally:
+        # Cleanup and recover file descriptors
+        os.dup2(old_stderr_fd, 2)
+        stderr_tempfile.close()
+        if data is not None:
+            os.dup2(old_stdin_fd, 0)
+            stdin_tempfile.close()
+
     return output, errmsg
 
 @app.route('/', methods=["GET"])
@@ -59,10 +78,11 @@ def clickhouse():
     result, errmsg = chdb_query_with_errmsg(query.strip(), format)
     if len(errmsg) == 0:
         return result, 200
-    if len(result) > 0:
+    elif len(result) > 0:
         print("warning:", errmsg)
         return result, 200
-    return errmsg, 400
+    else:
+        return errmsg, 400
 
 @app.route('/', methods=["POST"])
 @auth.login_required
@@ -71,35 +91,38 @@ def play():
     body = request.get_data() or None
     format = request.args.get('default_format', default="TSV", type=str)
     database = request.args.get('database', default="", type=str)
+    data = None
 
     if query is None:
-        query = b""
+        if body is not None:
+            query = body
+            body = None
+        else:
+            query = b""
     else:
         query = query.encode('utf-8')
 
     if body is not None:
-        # temporary hack to flatten multilines. to be replaced with raw `--file` input
-        data = f""
-        request_lines = body.decode('utf-8').strip().splitlines(True)
-        for line in request_lines:
-           data += " " + line.strip()
-        body = data.encode('utf-8')
-        query = query + " ".encode('utf-8') + body
+        if query is None:
+            query = body # body.encode('utf-8')
+        else:
+            data = body
 
-    if not query:
+    if not query and not body:
         return "Error: no query parameter provided", 400
 
     if database:
         database = f"USE {database}; ".encode()
         query = database + query
 
-    result, errmsg = chdb_query_with_errmsg(query.strip(), format)
+    result, errmsg = chdb_query_with_errmsg(query.strip(), format, data)
     if len(errmsg) == 0:
         return result, 200
-    if len(result) > 0:
+    if len and len(result) > 0:
         print("warning:", errmsg)
         return result, 200
-    return errmsg, 400
+    else:
+        return errmsg, 400
 
 
 @app.route('/play', methods=["GET"])
